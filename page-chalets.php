b168fbc3c8155ec5cc71bf633c1bef9d972b4712
Fred <fredbrisson13@gmail.com>
2020-10-27 16:25:02 -0400

    chalets

--- page-chalets.php
+++ page-chalets.php
@@ -1,212 +1,49 @@
-
-<!DOCTYPE html>
-<html lang="Fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Club Lac des Sables et Paradis</title>
-
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-
-    <link href="css/main.css" rel="stylesheet">
-    <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
-          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-
-</head>
-<body>
-<header>
-    <div class="logo">
-        <a href="../Club-Lac-des-Sables-et-Paradis/src/index.html"><img src="../Club-Lac-des-Sables-et-Paradis/src/images/logo.png"/></a>
-
-    </div>
-
-    <div id="hamburger">
-        <div id="hamburger-content">
-            <nav>
-                <ul class="main-nav">
-                    <li><a class="link" href="../Club-Lac-des-Sables-et-Paradis/src/front-page.html">Accueil</a></li>
-                    <li><a class="link" href="../Club-Lac-des-Sables-et-Paradis/src/single.html">Activités</a></li>
-                    <li><a class="link" href="page-chalets.php">Chalets</a></li>
-                    <li><a class="link" href="../Club-Lac-des-Sables-et-Paradis/src/template_contenue.html">Informations</a></li>
-                    <li><a class="link" href="../Club-Lac-des-Sables-et-Paradis/src/index.html">Galerie</a></li>
-                    <li><a class="link" href="#">Nous joindre</a></li>
-                    <li><a class="link" href="#">EN</a></li>
-                </ul>
-            </nav>
-        </div>
-
-        <button id="hamburger-button"><i class="fas fa-bars"></i></button>
-
-        <div id="hamburger-sidebar">
-            <div id="hamburger-sidebar-header"></div>
-            <div id="hamburger-sidebar-body"></div>
-        </div>
-
-        <div id="hamburger-overlay"></div>
-    </div>
-</header>
+<?php /*template name: Chalets */?>
+<?php get_header(); ?>
 
 <section class="couleurbleue wrapper">
 
     <section class="contenu-chalets wrapper">
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet1.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #1</h1>
-                            <p>10 personnes</p>
-                            <p>Éloigné du Lac-des-Sables</p>
+
+        <?php $indexquerry2=1;
+
+        query_posts(array(
+            'post_type'=>'pW_chalet',
+            'order'=>'ASC',
+            'post_status'=>'publish',
+            'showposts'=>-1
+        ));
+
+        while(have_posts()) {
+            the_post();
+            ?>
+
+            <article>
+                <a href="<?php the_permalink(); ?>">
+                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo the_title(); ?>"/>
+                    <div class="tout">
+                        <div class="premier">
+                            <div>
+                                <h1><?php echo the_title(); ?></h1>
+                                <p>10 personnes</p>
+                                <p>Éloigné du Lac-des-Sables</p>
+                            </div>
+                        </div>
+                        <div class="deuxieme">
+                            <p>200$/nuit</p>
+                            <span class="material-icons">navigate_next</span>
                         </div>
                     </div>
-                    <div class="deuxieme">
-                        <p>200$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet2.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #2</h1>
-                            <p>2 personnes</p>
-                            <p>Éloigné du Lac-des-Sables</p>
-                        </div>
-                    </div>
-                    <div class="deuxieme">
-                        <p>50$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet3.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #3</h1>
-                            <p>4 personnes</p>
-                            <p>Éloigné du Lac-des-Sables</p>
-                        </div>
-                    </div>
-                    <div class="deuxieme">
-                        <p>100$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet4.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #4</h1>
-                            <p>6 personnes</p>
-                            <p>Près du Lac-des-Sables</p>
-                        </div>
-                    </div>
-                    <div class="deuxieme">
-                        <p>175$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet5.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #5</h1>
-                            <p>8 personnes</p>
-                            <p>Près du Lac-des-Sables</p>
-                        </div>
-                    </div>
-                    <div class="deuxieme">
-                        <p>175$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet6.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #6</h1>
-                            <p>16 personnes</p>
-                            <p>Près du Lac-des-Sables</p>
-                        </div>
-                    </div>
-                    <div class="deuxieme">
-                        <p>300$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/single.html">
-                <img src="images/chalet7.png" alt="chalet 1"/>
-                <div class="tout">
-                    <div class="premier">
-                        <div>
-                            <h1>Chalet #7</h1>
-                            <p>8 personnes</p>
-                            <p>Près du Lac Caribou</p>
-                        </div>
-                    </div>
-                    <div class="deuxieme">
-                        <p>250$/nuit</p>
-                        <span class="material-icons">navigate_next</span>
-                    </div>
-                </div>
-            </a>
-        </article>
+                </a>
+            </article>
+            <?php
+            $indexquerry2++;
+        }
+        wp_reset_query();
+        ?>
+
     </section>
 
 </section>
 
-<footer>
-    <div class="wrapper">
-        <div class="footer-flex">
-            <a href="../Club-Lac-des-Sables-et-Paradis/src/front-page.html"><img class="logo-footer" src="../Club-Lac-des-Sables-et-Paradis/src/images/logo.png" alt="Logo"></a>
-            <div class="footer-contenu">
-                <div class="footer-text">
-                    <p>418 232-2020 (Louise ou Hélène) ou 418 233-3375 (15 mai à la fermeture)</p>
-                    <p>info@clublacdessables.com</p>
-                </div>
-                <div class="footer-media">
-                    <a href="https://www.facebook.com"><img src="../Club-Lac-des-Sables-et-Paradis/src/images/facebook.png" alt="Facebook"></a>
-                    <a href="https://www.youtube.com"><img src="../Club-Lac-des-Sables-et-Paradis/src/images/yotube.png" alt="YouTube"></a>
-                </div>
-            </div>
-
-        </div>
-
-
-        <p>©2020</p>
-    </div>
-
-
-</footer>
-
-</body>
-<script src="../Club-Lac-des-Sables-et-Paradis/src/js/script.js"></script>
-</html>
+<?php get_footer();?>
