6dedfb7f91b78514d19f1a9275f9859d15b720c1
Fred <fredbrisson13@gmail.com>
2020-10-29 16:00:22 -0400

    2e maj chalets

--- page-chalets.php
+++ page-chalets.php
@@ -20,7 +20,7 @@
 
             <article>
                 <a href="<?php the_permalink(); ?>">
-                    <img src="<?php the_field('pw_image_chalets') ?>" alt="<?php echo the_title(); ?>"/>
+                    <img src="<?php the_field('pw_image_chalet') ?>" alt="<?php echo the_title(); ?>"/>
                     <div class="tout">
                         <div class="premier">
                             <div>
