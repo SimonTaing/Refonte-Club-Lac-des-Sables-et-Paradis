0a00f7f29b9e1d7926124a59b27f67f035bd173c
stephane <stephanethibert01@gmail.com>
2020-10-28 20:44:19 -0400

    essaye carrousel

--- page-galerie.php
+++ page-galerie.php
@@ -1,52 +1,4 @@
-<!DOCTYPE html>
-<html lang="Fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Club Lac des Sables et Paradis</title>
-
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-
-    <link href="css/main.css" rel="stylesheet">
-    <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
-          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/glide.core.min.css">
-    <link rel="stylesheet" href="css/glide.theme.css">
-
-</head>
-<body>
-<header>
-    <div class="logo">
-        <a href="front-page.html"><img src="images/logo.png"/></a>
-
-    </div>
-
-    <div id="hamburger">
-        <div id="hamburger-content">
-            <nav>
-                <ul class="main-nav">
-                    <li><a class="link" href="front-page.html">Accueil</a></li>
-                    <li><a class="link" href="single.html">Activités</a></li>
-                    <li><a class="link" href="template_contenue.html">Informations</a></li>
-                    <li><a class="link" href="index.html">Galerie</a></li>
-                    <li><a class="link" href="#">Nous joindre</a></li>
-                    <li><a class="link" href="#">EN</a></li>
-                </ul>
-            </nav>
-        </div>
-
-        <button id="hamburger-button"><i class="fas fa-bars"></i></button>
-
-        <div id="hamburger-sidebar">
-            <div id="hamburger-sidebar-header"></div>
-            <div id="hamburger-sidebar-body"></div>
-        </div>
-
-        <div id="hamburger-overlay"></div>
-    </div>
-</header>
+<?php get_header(); ?>
 <section class="hero">
     <div class="containertexte">
         <h1>La Pourvoirie Club Lac des Sables et Paradis</h1>
@@ -73,50 +25,40 @@
 
         <div class="glide wrapper">
             <div class="glide__track" data-glide-el="track">
-                <ul class="glide__slides">
-                    <li class="glide__slide">
-
-                        <article>
-
-
-                            <img src="images/04.jpg"/>
-                        </article>
-
-                    </li>
-                    <li class="glide__slide">
-
-                        <article >
+                <?php
+        $rows = get_field('carrousel');
+            if( $rows ) {    echo ' <ul class="glide__slides">';
 
 
-                            <img src="images/06.jpg"/>
+                foreach( $rows as $row ) {
+                $image = $row['image'];
+
+
+                echo'<li class="glide__slide">
+
+
+                             <article>';
+
+             echo wp_get_attachment_image( $image, 'full' );echo '
+ 
+             </article>
+ </li>
+ 
+   </ul>
+ 
+                ';}}
 
 
 
-                        </article>
+          
 
-                    </li>
-
-                    <li class="glide__slide">
-
-                        <article >
+                ?>
 
 
-                            <img src="images/07.jpg" ></article>
 
-                    </li>
-                    <li class="glide__slide">
-
-                        <article >
+  </div>
 
 
-                            <img src="images/08.jpg" ></article>
-
-                    </li>
-
-
-                    </li>
-                </ul>
-            </div>
 
             <div class="glide__arrows" data-glide-el="controls">
                 <button class="glide__arrow glide__arrow--left" data-glide-dir="<"><i class="fas fa-arrow-left"></i>
@@ -163,31 +105,4 @@
 
 
 
-<footer>
-    <div class="wrapper">
-        <div class="footer-flex">
-            <a href="front-page.html"><img class="logo-footer" src="images/logo.png" alt="Logo"></a>
-            <div class="footer-contenu">
-                <div class="footer-text">
-                    <p>418 232-2020 (Louise ou Hélène) ou 418 233-3375 (15 mai à la fermeture)</p>
-                    <p>info@clublacdessables.com</p>
-                </div>
-                <div class="footer-media">
-                    <a href="https://www.facebook.com"><img src="images/facebook.png" alt="Facebook"></a>
-                    <a href="https://www.youtube.com"><img src="images/yotube.png" alt="YouTube"></a>
-                </div>
-            </div>
-
-        </div>
-
-
-        <p>©2020</p>
-    </div>
-
-
-</footer>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
-
-<script src="js/script.js"></script>
-</html>
+<?php get_footer(); ?>
--- page-information.php
+++ page-information.php
@@ -1,52 +1,4 @@
-<!DOCTYPE html>
-<html lang="Fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Club Lac des Sables et Paradis</title>
-
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-
-    <link href="css/main.css" rel="stylesheet">
-    <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
-          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/glide.core.min.css">
-    <link rel="stylesheet" href="css/glide.theme.css">
-
-</head>
-<body>
-<header>
-    <div class="logo">
-        <a href="front-page.html"><img src="images/logo.png"/></a>
-
-    </div>
-
-    <div id="hamburger">
-        <div id="hamburger-content">
-            <nav>
-                <ul class="main-nav">
-                    <li><a class="link" href="front-page.html">Accueil</a></li>
-                    <li><a class="link" href="single.html">Activités</a></li>
-                    <li><a class="link" href="template_contenue.html">Informations</a></li>
-                    <li><a class="link" href="index.html">Galerie</a></li>
-                    <li><a class="link" href="#">Nous joindre</a></li>
-                    <li><a class="link" href="#">EN</a></li>
-                </ul>
-            </nav>
-        </div>
-
-        <button id="hamburger-button"><i class="fas fa-bars"></i></button>
-
-        <div id="hamburger-sidebar">
-            <div id="hamburger-sidebar-header"></div>
-            <div id="hamburger-sidebar-body"></div>
-        </div>
-
-        <div id="hamburger-overlay"></div>
-    </div>
-</header>
+<?php get_header(); ?>
 <section class="hero">
     <div class="containertexte">
         <h1>La Pourvoirie Club Lac des Sables et Paradis</h1>
@@ -100,31 +52,5 @@
 
 
 
-<footer>
-    <div class="wrapper">
-        <div class="footer-flex">
-            <a href="front-page.html"><img class="logo-footer" src="images/logo.png" alt="Logo"></a>
-            <div class="footer-contenu">
-                <div class="footer-text">
-                    <p>418 232-2020 (Louise ou Hélène) ou 418 233-3375 (15 mai à la fermeture)</p>
-                    <p>info@clublacdessables.com</p>
-                </div>
-                <div class="footer-media">
-                    <a href="https://www.facebook.com"><img src="images/facebook.png" alt="Facebook"></a>
-                    <a href="https://www.youtube.com"><img src="images/yotube.png" alt="YouTube"></a>
-                </div>
-            </div>
 
-        </div>
-
-
-        <p>©2020</p>
-    </div>
-
-
-</footer>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
-
-<script src="js/script.js"></script>
-</html>
+<?php get_footer(); ?>
